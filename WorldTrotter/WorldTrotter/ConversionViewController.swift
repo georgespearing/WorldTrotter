2432539eed3a9f79879c79c80d97be8dca4db9e5
George Spearing <george.spearing@uvm.edu>
2021-11-27 10:16:56 -0500

    Initialize project and add tab view with two views

---
+++ WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by user203264 on 11/27/21.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("ConversionViewController loaded its view")
+    }
+
+
+}
+
---
+++ WorldTrotter/WorldTrotter/MapViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by user203264 on 11/27/21.
+//
+
+import UIKit
+
+class MapViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("MapViewController loaded its view")
+    }
+    
+}
